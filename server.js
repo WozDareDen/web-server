1795ee9aea1c9d49bd8a2d9fd46c19b9d200ded7
WozDareDen <fsadot@free.fr>
2018-06-15 11:29:43 +0200

    web-server

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express'),
+    ejs = require('ejs');
+
+let app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/',(req, res) => {
+    res.render('home.ejs',{
+        pageTitle: 'HomePage',
+        welcomeMessage: 'Bienvenue  sur mon website!',
+        currentYear: new Date().getFullYear()
+    });
+});
+
+app.get('/about',(req,res) => {
+    res.render('about.ejs',{
+        pageTitle: 'About Page',
+        currentYear: new Date().getFullYear()
+    });
+});
+
+
+app.get('/bad',(req,res) => {
+    res.send({
+        code: 69,
+        status: 'error message',
+        description: 'bah, juste un test'
+    });
+});
+
+
+
+
+app.listen(3000, () => {
+    console.log('server is up on port 3000');
+});
